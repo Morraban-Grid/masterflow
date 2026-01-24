d8e34f73aad0276d0df7e9e9093afe6f2ddc4bb4
Morraban-Grid <andromeda.4i.1000@gmail.com>
2026-01-24 16:34:33 -0500

    feat(transform): add transactions data cleaning and business rules

---
+++ src/transformers/transactions_transformer.py
@@ -0,0 +1,100 @@
+import pandas as pd
+import numpy as np
+
+from core.transformer import Transformer # Importa la clase base Transformer
+
+class TransactionsTransformer(Transformer):
+    # Tranformer encargado de limpiar, tipar y aplicar reglas de negocio sobre
+    # los datos de transacciones financieras.
+
+    # Esta clase hereda de Transformer, que es la clase base para todos los
+    # transformadores en el sistema.
+
+    def transform(self, df: pd.DataFrame) -> pd.DataFrame:
+        # Ejecuta todas las transformaciones necesarias sobre el DataFrame de transacciones.
+
+        df = df.copy()
+
+        df = self._normalize_column_names(df)
+        df = self._convert_data_types(df)
+        df = self._apply_business_rules(df)
+        df = self._remove_invalid_records(df)
+
+        return df
+    
+    @staticmethod
+    def _normalize_column_names(df: pd.DataFrame) -> pd.DataFrame:
+        # Normaliza los nombres de las columnas para evitar inconsistencias:
+        # - minúsculas
+        # - snake_case esto hace que los nombres de las columnas sean consistentes y fáciles de manejar.
+        # - sin caracteres especiales
+
+        df.columns = (
+            df.columns
+            .str.strip()
+            .str.lower()
+            .str.replace(" ", "_")
+            .str.replace(r"[^\w_]", "", regex=True)
+        ) 
+
+        return df
+
+    @staticmethod
+    def _convert_data_types(df: pd.DataFrame) -> pd.DataFrame:
+        # Convierte las columnas a los tipos de datos correctos
+        df["transaction_date"] = pd.to_datetime(
+            df["transaction_date"],
+            errors="coerce"
+        )
+
+        numeric_columns = [
+            "quantity",
+            "price",
+            "discount_applied",
+            "total_amount",
+        ]
+
+        for column in numeric_columns:
+            df[column] = pd.to_numeric(df[column], errors="coerce")
+
+        return df
+    
+    @staticmethod
+    def _apply_business_rules(df: pd.DataFrame) -> pd.DataFrame:
+        # Aplica reglas de negocio definidas para las transacciones financieras.
+
+        # Regla 1: El descuento debe estar entre 0 y 100
+        invalid_discount_mask = (
+            (df["discount_applied"]<0) |
+            (df["discount_applied"]>100)
+        )
+
+        df.loc[invalid_discount_mask, "discount_applied"] = np.nan
+
+        # Regla 2: Recalcular el total_amount
+        df["calculated_total_amount"] = (
+            df["quantity"] * df["price"]
+        ) * (1 - df["discount_applied"]/100)
+
+        return df
+    
+    @staticmethod
+    def _remove_invalid_records(df: pd.DataFrame) -> pd.DataFrame:
+        # Elimina registros que no cumplen criterios mínimos de calidad
+
+        df = df.dropna(
+            subset=[
+                "customer_id",
+                "product_id",
+                "transaction_date",
+                "quantity",
+                "price",
+                "calculated_total_amount",
+            ]
+        )
+
+        df = df[df["quantity"]>0]
+        df = df[df["price"]>0]
+
+        return df 
+
